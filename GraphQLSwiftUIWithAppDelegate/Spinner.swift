4ab619ab1dcdc6f7e0a95a82dd0dc98f9c10b406
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-17 17:00:33 +0300

    api

---
+++ GraphQLSwiftUIWithAppDelegate/Spinner.swift
@@ -0,0 +1,24 @@
+//
+//  Spinner.swift
+//  GraphQLSwiftUIWithAppDelegate
+//
+//  Created by Ramill Ibragimov on 17.05.2021.
+//
+
+import SwiftUI
+import UIKit
+
+struct Spinner: UIViewRepresentable {
+    let style: UIActivityIndicatorView.Style
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: style)
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        return spinner
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        
+    }
+}
